edb96657695217df0713ac71f905d5b00a9376fe
Jaquellynne <51082468+Jaquellynne@users.noreply.github.com>
2021-10-16 11:55:11 -0300

    Add files via upload

---
+++ Atividade III/CÓDIGO/Fornecedor/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Facades\Route;
+use app\Http\Controllers\FornecedorController;
+
+/*
+|-----------------------------------------------------------------------
+| Web Routes
+|-----------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+
+ Route::get('/index', [FornecedorController::class, 'index']);
+
+ Route::get('/create', [FornecedorController::class, 'create']);
+
+ Route::post('/store', [FornecedorController::class, 'store']);
+
+
+
