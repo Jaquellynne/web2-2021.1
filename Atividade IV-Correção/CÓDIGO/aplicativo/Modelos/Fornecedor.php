19843914ee12cc667cb739958c9bf27a07095128
Jaquellynne <51082468+Jaquellynne@users.noreply.github.com>
2021-11-01 16:15:06 -0300

    Add files via upload

---
+++ Atividade IV-Correção/CÓDIGO/aplicativo/Modelos/Fornecedor.php
@@ -0,0 +1,27 @@
+<?php
+    
+
+    namespace app\Models;
+
+    use Illuminate\Database\Eloquent\Factories\MasFactory;
+    use Illuminate\Database\Eloquent\Models;
+
+    class Fornecedor extends Models {
+
+        use MasFactory;
+
+        //nome da tabela
+        protected $table = "fornecedor";
+
+        //colocar campos que serao setados no bd
+        protected $fillable = [
+            'nome',
+            'CNPJ',
+            'email'
+
+            ];
+
+        function Entrada(){
+            return $this->belongsTo(Entrada::class, 'entrada_id', 'id');
+        }    
+    }
