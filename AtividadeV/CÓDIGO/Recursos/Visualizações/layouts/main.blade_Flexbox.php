064a72fdaa708254e41ea82564b10bb3eb9c6824
Jaquellynne <51082468+Jaquellynne@users.noreply.github.com>
2021-11-02 12:05:23 -0300

    Add files via upload

---
+++ AtividadeV/CÓDIGO/Recursos/Visualizações/layouts/main.blade_Flexbox.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- CSS local -->
+    <link rel="stylesheet" href="../css/style_layout2.css">
+    <title>Sistema de vendas</title>
+	<header>
+      <h1>Sistema de vendas</h1>
+    </header>
+
+    <section>
+      <article>
+        <h2>Cadastro de clientes</h2>
+        <p> No cadastro de clientes é possível cadastrar os seguintes dados: Nome do cliente, débito e descrição.</p>
+      </article>
+
+      <article>
+        <h2>Cadastro de Produtos</h2>
+        <p> No cadastro de clientes é possível cadastrar os seguintes dados: Nome do produto e tipo do produto.</p>
+      </article>
+
+      <article>
+        <h2>Cadastro de Fornecedores</h2>
+        <p>No cadastro de clientes é possível cadastrar os seguintes dados: Nome do fornecedor, CNPF e email.</p>
+    </
+ </head>    
+</html>
---
+++ AtividadeV/CÓDIGO/Recursos/Visualizações/layouts/main.blade_NormalFlow.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- CSS local -->
+    <link rel="stylesheet" href="../css/style_layout1.css">
+
+    <h1>Sistema de Vendas</h1>
+
+    <p>Este sistema possui: </p>
+
+    <p>Cadastro de Produtos</p>
+
+    <p>Cadastro de Entrada</p>
+
+    <p>Cadastro de Fornecedores<span>Cadastro de ItensEntrada</span> e <span>Cadastro de ItensVendas</span> 
+    Cadastro de Vendas<span>Cadastro de Clientes</span>, todas as funcionalidades referentes a um sistema de vendas:
+     <img src="img/sistema.jpg"></p>
+</head>
+</html>
