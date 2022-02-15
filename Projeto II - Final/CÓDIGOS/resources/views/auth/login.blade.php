c2c3c1752dd7e61fa8d2e40229ac5a06f3bcb7ec
Jaquellynne <51082468+Jaquellynne@users.noreply.github.com>
2022-02-15 17:21:24 -0300

    Add files via upload

---
+++ Projeto II - Final/CÓDIGOS/resources/views/auth/login.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.app', [
+    'class' => 'login-page',
+    'backgroundImagePath' => 'img/bg/background-login.jpeg'
+])
+
+@section('content')
+    <div class="content">
+        <div class="container">
+            <div class="col-lg-4 col-md-6 ml-auto mr-auto">
+                <form class="form" method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="card card-login">
+                        <div class="card-header ">
+                            <div class="logo">
+                                <div class="simple-text logo-mini">
+                                    <div class="logo-image-small">
+                                        <img src="{{ asset('paper') }}/img/carsystem-logo.png">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body ">
+
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="nc-icon nc-email-85"></i>
+                                    </span>
+                                </div>
+                                <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}" type="email" name="email" value="{{ old('email') }}" required autofocus>
+                                
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" style="display: block;" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="nc-icon nc-key-25"></i>
+                                    </span>
+                                </div>
+                                <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="{{ __('Password') }}" type="password" required>
+                                
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" style="display: block;" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+
+                            {{-- <div class="form-group">
+                                <div class="form-check">
+                                     <label class="form-check-label">
+                                        <input class="form-check-input" name="remember" type="checkbox" value="" {{ old('remember') ? 'checked' : '' }}>
+                                        <span class="form-check-sign"></span>
+                                        {{ __('Remember me') }}
+                                    </label>
+                                </div>
+                            </div> --}}
+                        </div>
+
+                        <div class="card-footer">
+                            <div class="text-center">
+                                <button type="submit" style="background-color: #006494" class="btn btn-round mb-3">{{ __('Entrar') }}</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+              
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            demo.checkFullPageBackgroundImage();
+        });
+    </script>
+@endpush
