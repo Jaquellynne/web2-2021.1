652447ed5f139057229f3921a29bb08ff113f850
Jaquellynne <51082468+Jaquellynne@users.noreply.github.com>
2022-02-15 16:35:56 -0300

    Create 2021_12_22_133110_create_users_table.php

---
+++ Projeto II - Final/CÓDIGOS/database/migrations/2021_12_22_133110_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->bigIncrements('id')->unsigned();
+			$table->string('name');
+			$table->integer('nivelAcesso');
+			$table->string('cpf', 14);
+			$table->string('foto', 240)->nullable();
+			$table->string('endereco', 80)->nullable();
+			$table->string('telefone', 15)->nullable();
+			$table->date('dataNascimento')->nullable();
+			$table->string('email')->unique();
+			$table->dateTime('email_verified_at')->nullable();
+			$table->string('password');
+			$table->string('remember_token', 100)->nullable();
+			$table->timestamps(6);
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
